931db3c608f7851ea0ca8b880e73d91419f02313
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-11-06 18:03:55 -0600

    commit tree 48a6371b7429ee65ab391e52c2464ba9f5f70591

--- src/index.js
+++ src/index.js
@@ -1,5 +1,58 @@
 const init = () => {
-  
+   const inputForm = document.querySelector('form')
+   inputForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    // console.log(event)
+    const input = document.querySelector('input#searchByID')
+    // console.log(input.value)
+
+    // const movies = 'http://localhost:3000/movies'
+    fetch('http://localhost:3000/movies')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+})
 }
 
-document.addEventListener('DOMContentLoaded', init);
+  
+// document.addEventListener('DOMContentLoaded', init);
+
+// const init = () => {
+//     const inputForm = document.querySelector('form')
+
+//     inputForm.addEventListener('submit', (event) => {
+//         event.preventDefault()
+//         const input = document.querySelector('input#searchByID')
+
+//         const movies = `http://localhost:3000/movies/${input.value}`
+//         fetch(movies)
+//         .then(response => response.json())
+//         .then(data =>{
+//             const title = document.querySelector('section#movieDetails h4')
+//             const summary = document.querySelector('section#movieDetails p')
+//             title.innerText = data.title
+//             summary.innerText = data.summary
+//         })
+//     })
+// }
+
+// document.addEventListener('DOMContentLoaded', init)
+
+// const init = () => {
+//     const inputForm = document.getElementById('searchByID')
+//     inputForm.addEventListener('submit', (event) =>{
+//         event.preventDefault()
+//         const input = document.getElementById('searchByID')
+
+//         const movies = `http://localhost:3000/movies/${input.value}`
+//         fetch(movies)
+//         .then(response => response.json())
+//         .then(data =>{
+//             const title = document.getElementById
+//         })
+//     })
+// }
+
+
+// document.addEventListener('DOMContentLoaded', init)
